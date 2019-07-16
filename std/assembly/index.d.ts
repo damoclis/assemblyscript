b232deef902abc521ea49bcf09334ab0ff3d3e76
强宇周 <qiangyuzhou@hyperchain.cn>
2019-07-16 16:38:05 +0800

    edit bytes

--- std/assembly/index.d.ts
+++ std/assembly/index.d.ts
@@ -1594,3 +1594,14 @@ declare class DataStream {
   writeString(str: string): void;
   writeDouble(d: f64): void;
 }
+
+declare class Bytes extends Uint8Array{ 
+  static fromHex(hex: string): Bytes;
+  static fromU8Array(arr: Array<u8>): Bytes;
+  toHex(): string;
+  swapEndian(): Bytes;
+  cloneBytes(): Bytes;
+  toU8Array(): Array<u8>;
+  concat(b2: Bytes): Bytes;
+  wrapDataStream(): DataStream;
+}
